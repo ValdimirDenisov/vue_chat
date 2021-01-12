45dd340babd8284fe8e07d19217468f9969e3ab4
Vladimir <vova_denisov_2003@mail.ru>
2021-01-12 19:48:29 +0300

    add template for chat

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <container>
+      <ChatWindow>
+        <ChatMessage v-bind:username="'Ivan'" v-bind:time="'21.12.2019 05:00:50'">Hello, World!!!</ChatMessage>
+      </ChatWindow>
+    </container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import container from './components/Container.vue'
+import ChatMessage from './components/ChatMessage.vue'
+import ChatWindow from './components/ChatWindow.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    container,
+    ChatMessage,
+    ChatWindow,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    margin: 0;
+    background-color: #f9f9fa;
+  }
 </style>
---
+++ src/components/Container.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <slot> </slot> 
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'container',
+    props: {
+      msg: String
+    }
+  }
+</script>
+
+
+<style>
+  .container {
+    width: 764px;
+    margin: auto;
+    box-shadow: 0 0 15px 0 #5f5f5f73;
+  }
+</style>
